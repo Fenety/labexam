b669a92d89ae73d9979925c3ba0ab9fa336a7a6b
Fenety <118210339+Fenety@users.noreply.github.com>
2024-05-13 16:41:43 +0300

    Add files via upload

---
+++ stack.java
@@ -0,0 +1,17 @@
+package stack;
+import java.util.Stack;
+public class stack{
+public static void main(String[] args){
+        Stack<String> stack=new Stack<String>();
+        System.out.println(stack.empty());
+        stack.push("abebe");
+        stack.push("kebede");
+        stack.push("feromsis");
+        stack.push("feven");
+        stack.push("beti");
+   System.out.println(stack);    
+   }
+  }
+
+
+
